4aa97190456edce10debefeef9f3f49591a0d105
zigorrom <https://github.com/zigorrom/DigitalAnalyzer.git>
2016-04-10 22:12:30 +0200

    1

---
+++ StepperParametersCalculation/StepperCalculation.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StepperParametersCalculation
+{
+    public class StepperCalculation
+    {
+        private int _colNum;
+        private int _rowNum;
+        private double _cellWidth;//in millimeters
+        private double _stageLengthPerStep;
+        private double _rotatorAnglePerStep;
+        private double _stageCenterToDispencerLength;
+        private int _stepsFromCenterToDispencer;
+        private double _midColIndex;
+        private double _midRowIndex;
+
+
+        //optimize - 4 time same info
+        private int[,] StageSteps;
+
+        private int[,] AngleSteps;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="ColNum"> Number of columns</param>
+        /// <param name="RowNum">Number of rows</param>
+        /// <param name="CellWidth">Width of single cell in millimeters </param>
+        /// <param name="StageLengthPerStep">Length which stage moves per single step</param>
+        /// <param name="RotatorAnglePerStep">Angle which stage rotates per single step</param>
+        /// <param name="StageCenterToDispencerLength">Default home distance between stage center and dispencing position in millimeters</param>
+        public StepperCalculation(int ColNum, int RowNum, double CellWidth, double StageLengthPerStep, double RotatorAnglePerStep, double StageCenterToDispencerLength)
+        {
+            _colNum = ColNum;
+            _rowNum = RowNum;
+            _cellWidth = CellWidth;
+            _stageLengthPerStep = StageLengthPerStep;
+            _rotatorAnglePerStep = RotatorAnglePerStep;
+            _stageCenterToDispencerLength = StageCenterToDispencerLength;
+            _midColIndex = (_colNum - 1.0) / 2;
+            _midRowIndex = (_rowNum - 1.0) / 2;
+            _stepsFromCenterToDispencer = (int)Math.Round(_stageCenterToDispencerLength / _stageLengthPerStep);
+
+            StageSteps = new int[_rowNum, _colNum];
+            AngleSteps = new int[_rowNum, _colNum];
+            InitArrays();
+        }
+
+
+
+        private void InitArrays()
+        {
+            for (int i = 0; i < _rowNum; i++)
+            {
+                for (int j = 0; j < _colNum; j++)
+                {
+                    StageSteps[i, j] = (int)Math.Round(LengthFromCellToStageCenter(j, i) / _stageLengthPerStep);
+                    AngleSteps[i, j] = (int)Math.Round(AngleFromCellToXaxis(j, i) / _rotatorAnglePerStep);
+                }
+            }
+        }
+
+
+        public int GetSteps(int Col, int Row)
+        {
+            if (Col < 0 || Col >= _colNum)
+                return 0;
+            if (Row < 0 || Row >= _rowNum)
+                return 0;
+            return _stepsFromCenterToDispencer - StageSteps[Row, Col];
+        }
+
+        public int GetAngles(int Col, int Row)
+        {
+            if (Col < 0 || Col >= _colNum)
+                return 0;
+            if (Row < 0 || Row >= _rowNum)
+                return 0;
+            return AngleSteps[Row, Col];
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="ColumnNumber">Zero-based column number</param>
+        /// <param name="RowNumber">Zero-based row number</param>
+        /// <returns>Length between zero-point of stage and current cell in millimeters </returns>
+
+        public double LengthFromCellToStageCenter(int ColumnNumber, int RowNumber)
+        {
+            if (ColumnNumber >= _colNum || ColumnNumber < 0)
+                return 0;
+            if (RowNumber >= _rowNum || RowNumber < 0)
+                return 0;
+            var colIndexLength = Math.Abs(ColumnNumber - _midColIndex);
+            var rowIndexLength = Math.Abs(RowNumber - _midRowIndex);
+            var totalLength = _cellWidth * Math.Sqrt(colIndexLength * colIndexLength + rowIndexLength * rowIndexLength); // in millimeters
+            return totalLength;
+        }
+
+        public double AngleFromCellToXaxis(int ColumnNumber, int RowNumber)
+        {
+            if (ColumnNumber >= _colNum || ColumnNumber < 0)
+                return 0;
+            if (RowNumber >= _rowNum || RowNumber < 0)
+                return 0;
+            var x = ColumnNumber - _midColIndex;
+            var y = RowNumber - _midRowIndex;
+            var angle = -1 * Math.Atan2(y, x) * (180 / Math.PI);
+            if (angle < 0)
+                angle = 360 + angle;
+            return angle;
+        }
+
+        public int RotatorHalfCircleSteps
+        {
+            get { return (int)Math.Round(180 / _rotatorAnglePerStep); }
+        }
+
+        public int RotatorFullCircleSteps
+        {
+            get { return (int)Math.Round(360 / _rotatorAnglePerStep); }
+        }
+
+
+        public int StageMovementSteps(int ColumnNumber, int RowNumber, int PrevColumnNumber, int PrevRowNumber)
+        {
+            var prevXsteps = GetSteps(PrevColumnNumber,PrevRowNumber);
+            var currXsteps = GetSteps(ColumnNumber, RowNumber);
+            var stepsToMove = currXsteps - prevXsteps;
+            return stepsToMove;
+        }
+
+        public int StageRotationSteps(int ColumnNumber, int RowNumber, int PrevColumnNumber, int PrevRowNumber)
+        {
+            var prevAngle = GetAngles( PrevColumnNumber,PrevRowNumber);
+            var currAngle = GetAngles(ColumnNumber, RowNumber);
+            var stepToMove = currAngle - prevAngle;
+            if (stepToMove > RotatorHalfCircleSteps)
+                stepToMove = stepToMove - RotatorFullCircleSteps;
+            return stepToMove;
+
+        }
+
+    }
+}
---
+++ StepperTest/Program.cs
@@ -0,0 +1,58 @@
+﻿using StepperParametersCalculation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StepperTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cols = 5;
+            var rows = 5;
+            var a = new StepperCalculation(cols, rows, 30, 0.2825,7.5, 900);
+            Console.WriteLine("****************Stage*******************");
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    Console.Write("{0}\t",a.GetSteps(j,i));
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("****************Angle*******************");
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    var agl = a.GetAngles(j, i);
+                    //if (agl > a.RotatorHalfCircleSteps)
+                    //    agl = agl - a.RotatorFullCircleSteps;
+                    Console.Write("{0}\t", agl);
+                }
+                Console.WriteLine();
+            }
+
+
+            Console.WriteLine("****************Motion*******************");
+            int x1=0,y1=0,x2=1,y2=1;
+            Console.WriteLine("From x={0},y={1} to x={2}, y={3} steps = {4}",x1,y1,x2,y2, a.StageMovementSteps(x2, y2, x1, y1));
+            Console.WriteLine("**************AngleMotion****************");
+            Console.WriteLine("Half circle steps = {0}",a.RotatorHalfCircleSteps);
+            Console.WriteLine("Full circle steps = {0}", a.RotatorFullCircleSteps);
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    Console.Write("{0}\t",a.StageRotationSteps(j,i,-1,-1));
+                }
+                Console.WriteLine();
+            }
+
+
+        }
+    }
+}
